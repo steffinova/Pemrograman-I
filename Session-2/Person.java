64fafebeb6dd04a65994d51e3abe3766934f5a39
steffinova <steffinova92@gmail.com>
2012-10-09 09:28:48 +0700

    Pesan

---
+++ Session-2/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+
+    public static void main(String[] args) {
+	String name = "Steffi Nova";
+	String nip = "4512212043";
+        System.out.println("Hello, my name is : " + name);
+	System.out.println("NIP : " + nip);
+    }
+
+}
